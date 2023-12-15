a650aabba9de9e182d8a829679fcba8722bed3a6
maso0310 <36530475+maso0310@users.noreply.github.com>
2023-12-15 15:02:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
+import threading
+import time
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
+def wake_up():
+    while True:
+        print("执行唤醒任务...")
+        # 在这里执行需要的唤醒操作
+        # 比如请求自己的Flask应用以保持活跃
+
+        # 每10分钟（600秒）执行一次
+        time.sleep(600)
+
+# 创建一个后台线程来执行唤醒任务
+wake_up_thread = threading.Thread(target=wake_up)
+wake_up_thread.daemon = True  # 设置为守护线程
+wake_up_thread.start()
 
 @app.route("/<path:path>", methods=['GET', 'POST', 'PUT', 'DELETE'])
 def index(path):
